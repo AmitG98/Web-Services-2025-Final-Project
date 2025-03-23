1e512b423a7d8060d15b4d0444bd1a99e8119e02
AmitG98 <amit1gordon1@gmail.com>
2025-03-23 13:21:32 +0200

    Building server-side infrastructure - connect all the modules

--- server/server.js
+++ server/server.js
@@ -1,19 +1,40 @@
 const express = require('express');
-const app = express();
+const cookieParser = require('cookie-parser');
+const cors = require('cors');
 const logger = require('morgan');
+const connectDB = require('./db/connection_db');
+const authRoutes = require('./routes/authRoutes');
+const userRoutes = require('./routes/userRoutes');
+const programRoutes = require('./routes/programRoutes');
+const reviewRoutes = require('./routes/reviewRoutes');
+const errorHandler = require('./middleware/errorHandler');
+
 const port = process.env.PORT || 8080;
 
+const app = express();
+
+connectDB();
+
+app.use(cors({ origin: true, credentials: true }));
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
+app.use(cookieParser());
 app.use(logger("dev"));
 
 app.get('/', (req, res) => {
     res.send('Welcome');
 });
 
+app.use('/api/auth', authRoutes);
+app.use('/api/users', userRoutes);
+app.use('/api/programs', programRoutes);
+app.use('/api/reviews', reviewRoutes);
+
 app.use((req, res) => {
     res.status(404).send("Page wasn't found");
 });
+
+app.use(errorHandler);
 
 app.listen(port, () => {
     console.log(`Listening on: http://localhost:${port}`);
