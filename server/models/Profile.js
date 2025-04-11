cf096029c74a979396253fe25814fa0b9f4c8724
AmitG98 <amit1gordon1@gmail.com>
2025-04-11 17:39:53 +0300

    create Profile model with scheme

---
+++ server/models/Profile.js
@@ -0,0 +1,27 @@
+const mongoose = require('mongoose');
+
+const { Schema, model } = require('mongoose');
+
+const profileSchema = new Schema(
+  {
+    user: {
+      type: Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
+    name: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+    avatar: {
+      type: String,
+      default: "",
+    },
+    language: {
+      type: String,
+      default: "en",
+    },
+  },{ timestamps: true});
+
+module.exports = model('Profile', profileSchema);
