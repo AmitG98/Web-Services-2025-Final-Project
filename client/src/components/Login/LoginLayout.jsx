2d731e697f39722d51fc70e1e250e294ef94ae1b
Mayabenhous2001@gmail.com <maya.benhous@stu.shenkar.ac.il>
2025-04-13 12:36:59 +0300

    update login layout - location of footer

--- client/src/components/Login/LoginLayout.jsx
+++ client/src/components/Login/LoginLayout.jsx
@@ -2,17 +2,16 @@ import React from "react";
 
 const LoginLayout = ({ children }) => {
   return (
-    <div
-      className="relative min-h-screen overflow-x-hidden bg-[rgb(20,20,20)] bg-cover bg-center"
-      style={{ backgroundImage: "url('/BannerEnter.png')" }}
-    >
+    <div className="flex flex-col min-h-screen bg-[rgb(20,20,20)] bg-cover bg-center overflow-x-hidden relative" style={{ backgroundImage: "url('/BannerEnter.png')" }}>
       <div className="absolute inset-0 bg-black opacity-50 z-10"></div>
       <img
         src="/Logo.png"
         alt="Netflix Logo"
         className="w-28 h-10 absolute top-10 left-20 right-0 z-20"
       />
-      <div className="relative z-30">{children}</div>
+      <div className="relative z-30 flex-grow flex flex-col">
+        {children}
+      </div>
     </div>
   );
 };
