f6d42536615f948a6c438b925b6fd89eedef7716
AmitG98 <amit1gordon1@gmail.com>
2025-04-14 18:15:20 +0300

    review fetch from server to client

---
+++ client/src/api/review.js
@@ -0,0 +1,10 @@
+import axios from './axiosInstance';
+
+export const addReview = (data) => axios.post('/api/reviews', data);
+export const getReviewsByProgram = (programId) => axios.get(`/api/reviews/program/${programId}`);
+export const getMyReviews = () => axios.get('/api/reviews/my-reviews');
+export const updateReview = (reviewId, data) => axios.put(`/api/reviews/${reviewId}`, data);
+export const deleteReview = (reviewId) => axios.delete(`/api/reviews/${reviewId}`);
+export const likeReview = (reviewId) => axios.post(`/api/reviews/${reviewId}/like`);
+export const getTopRated = () => axios.get('/api/reviews/top-rated');
+export const getMostReviewed = () => axios.get('/api/reviews/most-reviewed');
