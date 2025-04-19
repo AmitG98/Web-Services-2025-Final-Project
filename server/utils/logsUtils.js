55a80e1dfcf58680230d55e58dd0ae019ea69c81
AmitG98 <amit1gordon1@gmail.com>
2025-04-19 22:04:12 +0300

    add getStartOfToday function

--- server/utils/logsUtils.js
+++ server/utils/logsUtils.js
@@ -1,20 +1,25 @@
 const Log = require("../models/Log");
 
-const logOncePerDay = async (userId, action, details = {}) => {
-const alreadyExists = await Log.findOne({
-    user: userId,
-    action,
-    createdAt: { $gte: getStartOfToday() },
-});
-
-if (!alreadyExists) {
-    await Log.create({
-    user: userId,
-    action,
-    level: "info",
-    ...details,
-    });
+function getStartOfToday() {
+    const now = new Date();
+    return new Date(now.getFullYear(), now.getMonth(), now.getDate());
 }
+
+const logOncePerDay = async (userId, action, details = {}) => {
+    const alreadyExists = await Log.findOne({
+        user: userId,
+        action,
+        createdAt: { $gte: getStartOfToday() },
+    });
+
+    if (!alreadyExists) {
+        await Log.create({
+        user: userId,
+        action,
+        level: "info",
+        ...details,
+        });
+    }
 };
 
 module.exports = {logOncePerDay }
