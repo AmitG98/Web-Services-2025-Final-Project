8227466e3e97db15e5383bb6608b99e5460ce98a
Mayabenhous2001@gmail.com <maya.benhous@stu.shenkar.ac.il>
2025-05-07 11:34:19 +0300

    fix final cors in app.js

--- server/app.js
+++ server/app.js
@@ -20,13 +20,9 @@ const app = express();
 
 app.use("/public", express.static(path.join(__dirname, "public")));
 
-// app.use(cors({ origin: ["http://localhost:3000","https://web-services-2025-final-project.netlify.app"], credentials: true }));
 app.use(cors({
   origin: [
     "http://localhost:3000",
-    "https://web-services-2025-final-project.netlify.app",
-    "https://webservices-2025-final-project.onrender.com",
-    "https://webservices-2025-final-project.netlify.com",
     "https://webservices2025finalproject.vercel.app"
   ],
   credentials: true
